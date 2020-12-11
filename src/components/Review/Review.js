de954896be8a2137474bf7a855131073c72ad385
Steven Maloney <steven.maloney@gmail.com>
2020-12-10 18:34:29 -0600

    basic file structure established

---
+++ src/components/Review/Review.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Review extends Component {
+    render() {
+        return (
+            <p>Hi from Review!</p>
+        )
+    }
+}
+
+export default Review;
---
+++ src/components/Success/Success.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Success extends Component {
+    render() {
+        return (
+            <p>Hi from Success!</p>
+        )
+    }
+}
+
+export default Success;
