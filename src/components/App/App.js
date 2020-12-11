9714d622d53e3e090d30c73a67ff8e54e3292c9d
Steven Maloney <steven.maloney@gmail.com>
2020-12-10 18:53:55 -0600

    routes seem effective for questions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,25 @@ import axios from 'axios';
 import './App.css';
 import Header from '../Header/Header';
 import Feeling from '../Feeling/Feeling';
+import { HashRouter as Router, Route } from "react-router-dom";
+import Understanding from '../Understanding/Understanding';
+import Supported from '../Supported/Supported';
+import Comments from '../Comments/Comments';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <br/>
-        <Feeling />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <br/>
+
+          <Route exact path='/' component={Feeling}/>
+          <Route path ='/understand' component={Understanding}/>
+          <Route path ='/support' component={Supported}/>
+          <Route path ='/comment' component={Comments}/>
+        </div>
+      </Router>
     );
   }
 }
